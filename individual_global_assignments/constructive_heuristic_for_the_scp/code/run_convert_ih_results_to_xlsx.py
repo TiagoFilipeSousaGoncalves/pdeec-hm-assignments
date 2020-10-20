4fd4ede63840b31b195f2cfe473513f64676666c
Tiago Gonçalves <tiago.galves@hotmail.com>
2020-10-20 22:57:35 +0100

    IH Excel Results Added

--- individual_global_assignments/constructive_heuristic_for_the_scp/code/run_convert_ih_results_to_xlsx.py
+++ individual_global_assignments/constructive_heuristic_for_the_scp/code/run_convert_ih_results_to_xlsx.py
@@ -17,7 +17,11 @@ if len(results_npy_files) == 40:
     for idx, results_file in enumerate(results_npy_files):
         results_array = np.load(file=os.path.join(results_dir, results_file), allow_pickle=True)
         results_df = pd.DataFrame(data=results_array, columns=[c for c in results_array[0, :]])
-        results_df.to_excel(excel_writer, sheet_name=results_file)
+        sheet_name = results_file.split('.')[0]
+        sheet_name = sheet_name.split('_')
+        sheet_name = sheet_name[0] +  sheet_name[1] + '_' + sheet_name[3] + '_' + sheet_name[6]
+        print(sheet_name)
+        results_df.to_excel(excel_writer, sheet_name=sheet_name)
     
     excel_writer.save()
 
