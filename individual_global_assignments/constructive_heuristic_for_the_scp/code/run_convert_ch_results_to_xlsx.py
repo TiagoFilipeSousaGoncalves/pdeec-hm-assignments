72b11b9604b12273e307ebfa93423ca3621b94a6
Tiago Gonçalves <tiago.galves@hotmail.com>
2020-10-21 01:38:18 +0100

    Processed Excel Files Added

--- individual_global_assignments/constructive_heuristic_for_the_scp/code/run_convert_ch_results_to_xlsx.py
+++ individual_global_assignments/constructive_heuristic_for_the_scp/code/run_convert_ch_results_to_xlsx.py
@@ -13,7 +13,7 @@ results_npy_files = [f for f in results_npy_files if not f.startswith('.')]
 
 # There should be 5 results files
 if len(results_npy_files) == 5:
-    excel_writer = pd.ExcelWriter(path=os.path.join(results_dir, 'results_excel.xlsx'), engine='xlsxwriter')
+    excel_writer = pd.ExcelWriter(path=os.path.join(results_dir, 'results_excel_ch.xlsx'), engine='xlsxwriter')
     for idx, results_file in enumerate(results_npy_files):
         results_array = np.load(file=os.path.join(results_dir, results_file), allow_pickle=True)
         results_df = pd.DataFrame(data=results_array, columns=[c for c in results_array[0, :]])
